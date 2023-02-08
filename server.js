1c903c0b260962c03e437bfbcf6ea930e01549db
ashley <alandis01@gmail.com>
2023-02-08 16:49:21 -0500

    added starter folders / files

---
+++ server.js
@@ -0,0 +1,26 @@
+
+{
+    "name": "socialnetworkapi-nosql",
+    "version": "1.0.0",
+    "description": "## User Story",
+    "main": "server.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1",
+      "seed": "node utils/seed"
+    },
+    "repository": {
+      "type": "git",
+      "url": "git+https://github.com/alandis01/Social-NetworkAPI-NoSQL.git"
+    },
+    "keywords": [],
+    "author": "",
+    "license": "ISC",
+    "bugs": {
+      "url": "https://github.com/alandis01/Social-NetworkAPI/issues"
+    },
+    "homepage": "https://github.com/alandis01/Social-NetworkAPIL#readme",
+    "dependencies": {
+      "express": "^4.18.2",
+      "mongoose": "^5.9.9"
+    }
+  }
