403d99cb0ae617b5cd7d3bf81e12d3e10b300abf
ashley <alandis01@gmail.com>
2023-02-08 22:13:14 -0500

    added delete user and add friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,13 +41,51 @@ const userController = {
 
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
-        .select('__v')
-        .populate('thoughts')
-        .populate('friends')
-        .then((users) => res.json (users))
+            .select('__v')
+            .populate('thoughts')
+            .populate('friends')
+            .then((users) => res.json(users))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this ID found!' })
+                    : Thought.findOneAndUpdate(
+                        { users: req.params.userId },
+                        { $pull: { users: req.params.userId } },
+                        { new: true },
+                    )
+            )
+            .then((thought) => 
+            !thought
+            ? res.status(404).json({ message: 'This user has been deleted with no thoughts found related' })
+            :res.json({ message: 'User sucesfully deleted!' })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId }},
+            { runValidators: true, new: true },
+        )
+        .then ((user) => 
+        !user
+        ?res.status(404).json({ message: 'No user with this ID found'})
+        : res.json(user)
+        )
         .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
+            res.status(500).json(err)
         });
     },
 
