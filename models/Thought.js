6a8223614c979f6a37c7e16624b3dae05a313d8b
ashley <alandis01@gmail.com>
2023-02-08 20:02:02 -0500

    created thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,40 @@
-const 
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thougthSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            defauolt: Date.now(),
+            get: value => value.toDateString()
+        },
+        username: {
+            type: String,
+            required: true 
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+);
+
+thougthSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions?.length;
+    });
+
+    const Thought = model('thought', thougthSchema);
+
+    module.exports = Thought; 
