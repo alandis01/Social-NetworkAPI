a8b096ea74d74dca5fec20bb645da1b47ce85555
ashley <alandis01@gmail.com>
2023-02-08 22:05:01 -0500

    created update and get single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,25 +4,54 @@ const userController = {
 
     getAllUsers(req, res) {
         User.find()
-        .select('__v')
-        .populate('thoughts')
-        .populate('friends')
-        .then((users) => res.json(users))
-        .catch((err) => {
-            console.log(err)
-            return res.status(500).json(err);
-        });
+            .select('__v')
+            .populate('thoughts')
+            .populate('friends')
+            .then((users) => res.json(users))
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).json(err);
+            });
     },
 
     createUser(req, res) {
         User.create(req.body)
-        .then((user) => res.json(user))
-        .catch ((err) => {
+            .then((user) => res.json(user))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this ID found!' })
+                    : res.json(user)
+            )
+            .catch((err) => {
+                res.status(500).json(err)
+            });
+    },
+
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select('__v')
+        .populate('thoughts')
+        .populate('friends')
+        .then((users) => res.json (users))
+        .catch((err) => {
             console.log(err);
             return res.status(500).json(err);
         });
     },
 
+    
 
 }
 
